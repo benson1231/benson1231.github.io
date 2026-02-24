4b8e1b2a914df6c070299ea9ce06b626f0c32d4d
benson lee <benson106075@gmail.com>
2026-02-24 14:26:03 +0800

    feat: fix

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Nav from './components/Nav.jsx'
+import ProfileCard from './components/ProfileCard.jsx'
+import AboutMe from './components/AboutMe.jsx'
+import Education from './components/Education.jsx'
+import Experience from './components/Experience.jsx'
+import Skills from './components/Skills.jsx'
+import Portfolio from './components/Portfolio.jsx'
+import Certifications from './components/Certifications.jsx'
+import Footer from './components/Footer.jsx'
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <ProfileCard />
+      <AboutMe />
+      <Education />
+      <Experience />
+      <Skills />
+      <Portfolio />
+      <Certifications />
+      <Footer />
+    </>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "./index.css";
-import Nav from './components/Nav.jsx'
-import ProfileCard from './components/ProfileCard.jsx'
-import AboutMe from './components/AboutMe.jsx'
-import Footer from './components/Footer.jsx';
-import Education from './components/Education.jsx';
-import Experience from './components/Experience.jsx';
-import Skills from './components/Skills.jsx';
-import Portfolio from './components/Portfolio.jsx';
-import Certifications from './components/Certifications.jsx';
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Nav/>
-    <ProfileCard/>
-    <AboutMe/>
-    <Education/>
-    <Experience />
-    <Skills />
-    <Portfolio />
-    <Certifications />
-    <Footer />
+    <App />
   </StrictMode>,
 )
